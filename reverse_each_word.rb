be843c286325e55e443c30df58323c2380ef9d37
Michelle Olivieri <michelleo72@gmail.com>
2018-03-20 23:15:28 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,4 @@
+=begin
 def reverse_each_word(sentence)
   array = sentence.split(" ")
   reverse_array = []
@@ -6,6 +7,7 @@ def reverse_each_word(sentence)
   end
     reverse_array.join(" ")
 end
+=end
 
 def reverse_each_word(sentence)
   array = sentence.split(" ")
