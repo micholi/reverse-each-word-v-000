f16cd0cde3982a1e185978723a60f2d1024688e6
Michelle Olivieri <michelleo72@gmail.com>
2018-03-20 22:45:16 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -10,9 +10,10 @@ end
 
 def reverse_each_word(sentence)
   array = sentence.split(" ")
+  reverse_array = []
     array.collect do |word|
       reverse_word = word.reverse
-      array << reverse_word
+      reverse_array << reverse_word
         array.join(" ")
   end
 end
